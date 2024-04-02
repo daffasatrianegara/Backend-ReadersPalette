c7ae7baa59b620a575872438d2ffa7660b3cc2b9
daffasatrianegara <daffasatria036@gmail.com>
2024-04-02 09:14:34 +0700

    refactor router photo

--- src/routes/photo.routes.js
+++ src/routes/photo.routes.js
@@ -10,7 +10,7 @@ const pathPhotoUsers = path.join(__dirname, '../../public/img/photo-profile')
 const pathPhotoBooks = path.join(__dirname, '../../public/img/book')
 
 router.put('/users/:id', uploadUser.single('file'), photoControllers.uploadPhotoUser)
-router.put('/book/:id', uploadBook.single('file'), photoControllers.uploadPhotoBooks)
+router.put('/books/:id', uploadBook.single('file'), photoControllers.uploadPhotoBooks)
 router.use('/users/:filename', (req, res) => {
     const filePath = path.join(pathPhotoUsers, req.params.filename)
     res.sendFile(filePath, (error) => {
@@ -20,7 +20,7 @@ router.use('/users/:filename', (req, res) => {
         }
     })
 })
-router.use('/book/:filename', (req, res) => {
+router.use('/books/:filename', (req, res) => {
     const filePath = path.join(pathPhotoBooks, req.params.filename)
     res.sendFile(filePath, (error) => {
         if(error) {
